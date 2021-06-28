536ca907639a6f0021ec8dca099c7d0c5991b055
Zoltan Kochan <z@kochan.io>
2021-06-28 13:45:19 +0300

    feat: support a --test flag

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,27 @@
 #!/usr/bin/env node
+import meow from 'meow'
 import update from '.'
 import path = require('path')
 
-update(path.resolve(process.argv[2])).catch((err) => {
+const cli = meow(`
+Usage
+  $ meta-updater <path to js>
+
+Options
+  --test  Do not update the files. Fail if updates are needed.
+
+Examples
+  $ meta-updater update.js
+  $ meta-updater update.js --test
+`, {
+  flags: {
+    test: {
+      type: 'boolean',
+    },
+  },
+})
+
+update(path.resolve(cli.input[0]), cli.flags).catch((err) => {
   console.error(err)
   process.exit(1)
 })
--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,35 @@ import { ProjectManifest } from '@pnpm/types'
 import loadJsonFile = require('load-json-file')
 import path = require('path')
 import exists = require('path-exists')
+import isEqual from 'ramda/src/isEqual'
 import writeJsonFile = require('write-json-file')
 
-export default async function (updaterFilePath: string) {
+export default async function (updaterFilePath: string, opts: { test?: boolean }) {
   const workspaceDir = await findWorkspaceDir(updaterFilePath)
   if (!workspaceDir) throw new Error(`${updaterFilePath} is not inside a workspace`)
   const updaterLib = await import(updaterFilePath)
   const updater = updaterLib.default ?? updaterLib
   const updateOptions = await updater(workspaceDir)
-  await performUpdates(workspaceDir, updateOptions)
+  await performUpdates(workspaceDir, updateOptions, opts)
 }
 
 async function performUpdates (
   workspaceDir: string,
-  update: Record<string, (obj: object, dir: string, manifest: ProjectManifest) => object | Promise<object>>
+  update: Record<string, (obj: object, dir: string, manifest: ProjectManifest) => object | Promise<object>>,
+  opts: { test?: boolean }
 ) {
   let pkgs = await findWorkspacePackages(workspaceDir, { engineStrict: false })
   for (const { dir, manifest, writeProjectManifest } of pkgs) {
     for (const [p, updateFn] of Object.entries(update)) {
       if (p === 'package.json') {
-        await writeProjectManifest(await updateFn(manifest, dir, manifest))
-        continue
+        const updatedManifest = await updateFn(manifest, dir, manifest)
+        const needsUpdate = isEqual(manifest, updatedManifest)
+        if (!needsUpdate) continue
+        if (!opts.test) {
+          await writeProjectManifest(updatedManifest)
+          continue
+        }
+        throw new Error(`package.json file in ${dir} is not up-to-date`)
       }
       if (!p.endsWith('.json')) continue
       const fp = path.join(dir, p)
@@ -32,7 +40,14 @@ async function performUpdates (
         continue
       }
       const obj = await loadJsonFile(fp)
-      await writeJsonFile(fp, await updateFn(obj as object, dir, manifest), { detectIndent: true })
+      const updatedObj = await updateFn(obj as object, dir, manifest)
+      const needsUpdate = isEqual(manifest, updatedObj)
+      if (!needsUpdate) continue
+      if (!opts.test) {
+        await writeJsonFile(fp, updatedObj, { detectIndent: true })
+        continue
+      }
+      throw new Error(`${fp} is not up-to-date`)
     }
   }
 }
