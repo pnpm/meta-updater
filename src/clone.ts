e833ada248066ed1566d6bab740a8cd133ff016e
Zoltan Kochan <z@kochan.io>
2023-08-02 18:03:28 +0300

    style: fix

--- src/clone.ts
+++ src/clone.ts
@@ -1,5 +1,4 @@
 import v8 from 'v8'
 
 export const clone =
-  // @ts-expect-error
   typeof structuredClone === 'function' ? structuredClone : <T>(value: T): T => v8.deserialize(v8.serialize(value))
